3c1585aabc3a8b80c4dfe36bcbd50ceb8be08f2c
damiadaraman <damiaadaraman@gmail.com>
2021-11-11 10:23:58 +0800

    Updated Header.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,16 @@
-import React from "react";
-import "./index.css";
-import Header from "./components/Header";
-import Navbar from "react-bootstrap/Navbar"; //Navigation from bootstrap
-import Nav from "react-bootstrap/Nav"; //Navigation from bootstrap
-import Footer from "./components/Footer";
-
+import React from 'react';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Navbar from 'react-bootstrap/Navbar'; //Navigation from bootstrap
+import Nav from 'react-bootstrap/Nav'; //Navigation from bootstrap
+import Footer from './components/Footer';
 
 //Parent component
 function App() {
   return (
-    <div className="App container py-3">
-      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        <Navbar.Brand className="font-weight-bold text-muted">
-          E Learning Website
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav activeKey={window.location.pathname}>
-            <Nav.Link>Subscribe</Nav.Link>
-            <Nav.Link>Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
+    <div className='App container py-3'>
+      <Header />
       <div>
         <Footer />
       </div>
