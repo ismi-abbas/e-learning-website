b02b5ea75fd01b0a5205cf5f13dac86dddb4b597
Abbas <muhdabbas98@gmail.com>
2021-11-12 00:20:03 +0800

    Create upload API

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -2,12 +2,14 @@ const upload = require('../../middleware/upload');
 const express = require('express');
 const router = express.Router();
 
-router.post('/upload', upload.single('file'), async (req, res) => {
-  if (req.file === undefined) return res.send('You must select a file.');
+router.post('/', upload.single('file'), async (req, res) => {
+  if (req.file === undefined) {
+    return res.send('You must select a file.');
+  }
   const imgUrl = `http://localhost:5001/api/upload/${req.file.filename}`;
   return res.send(imgUrl);
 });
 
-router.get('/', (req, res) => res.send('Profile route'));
+// router.get('/', (req, res) => res.send('Profile route'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
 const connectDB = require('./config/db');
 const express = require('express');
+const mongoose = require('mongoose');
+const Grid = require('gridfs-stream');
+
 // Init Express
 const app = express();
+
+// Init GFS
+let gfs;
+
 // Run connect database function
 connectDB();
 // Init middleware
@@ -15,7 +23,34 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/courses', require('./routes/api/courses'));
-app.use('/api/upload', require('./routes/api/upload'));
+app.use('/api/upload', require('./routes/api/upload')); // Upload route
+
+// Stream Grid-Fs
+// mongoose.connectDB('open', function () {
+//   gfs = Grid(conn.db, mongoose.mongo);
+//   gfs.collection('photos');
+// });
+
+// Media routes
+app.get('/api/upload/:filename', async (req, res) => {
+  try {
+    const file = await gfs.files.findOne({ filename: req.params.filename });
+    const readStream = gfs.createReadStream(file.filename);
+    readStream.pipe(res);
+  } catch (error) {
+    res.send('not found');
+  }
+});
+
+app.delete('/api/upload/:filename', async (req, res) => {
+  try {
+    await gfs.files.deleteOne({ filename: req.params.filename });
+    res.send('success');
+  } catch (error) {
+    console.log(error);
+    res.send('An error occured.');
+  }
+});
 
 // Store PORT in a variable
 const PORT = process.env.PORT || 5001; //process.env for deployment env
