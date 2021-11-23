5b9682eb8e36b68be37078640a11359b1d9f5cd2
Abbas <muhdabbas98@gmail.com>
2021-11-23 14:10:44 +0800

    Change pathing

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,17 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import Register from './components/auth/Register';
 import SubscribePlan from './components/layout/SubscribePlan';
-import ClientNavbar from './components/dashboard/user/ClientNavbar';
+import ClientNavbar from './components/Dashboard/User/ClientNavbar';
 import Login from './components/auth/Login';
 import Welcome from './components/layout/Welcome';
-import AdminHeader from './components/dashboard/admin/AdminHeader';
+import AdminHeader from './components/Dashboard/Admin/AdminHeader';
 import AdminMain from './components/layout/AdminMain';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Profile from './components/layout/Profile';
 import UserDashboard from '../src/components/page/user/UserDashboard';
 import UserLearning from '../src/components/page/user/UserLearning';
-import UploadCourse from './components/page/course/UploadCourse';
 import Landing from './components/layout/Landing';
-import ClientMain from './components/dashboard/user/ClientMain';
-import ClientDashboard from './components/dashboard/user/ClientDashboard';
-import UserMain from '../src/components//page/user/UserMain';
-import AdminUpload from './components/dashboard/admin/AdminUpload';
+import AdminUpload from './components/Dashboard/Admin/AdminUpload';
 
 // Redux
 import { Provider } from 'react-redux';
